9ee9b1a4b7b4186bd79a6b859759eaf0e45c8a0f
MykhailoK007 <kotmykhailo2000@gmail.com>
2020-08-12 03:17:14 +0300

    Updates

---
+++ precache-manifest.df1002ef7237e5defd182fe13dbeff10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd849203ad682404b25111c7b91562b8",
+    "url": "/test-announcement/index.html"
+  },
+  {
+    "revision": "a03af8f1b953276ddb28",
+    "url": "/test-announcement/static/css/main.6cb85b3c.chunk.css"
+  },
+  {
+    "revision": "66599d87c4c791c3d6fa",
+    "url": "/test-announcement/static/js/2.7aa73194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-announcement/static/js/2.7aa73194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03af8f1b953276ddb28",
+    "url": "/test-announcement/static/js/main.0d632936.chunk.js"
+  },
+  {
+    "revision": "0a4647cb7ef738d04f5b",
+    "url": "/test-announcement/static/js/runtime-main.c0319754.js"
+  }
+]);
